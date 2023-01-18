a8efd126822f12bf8c84e440dae94fb6b442e338
Mohit <mohitd2288@gmail.com>
2023-01-18 13:50:26 +0530

    added themes a

--- src/lib/components/Theme.svelte
+++ src/lib/components/Theme.svelte
@@ -1,18 +1,14 @@
 <script>
 	import Card from './Card.svelte';
 	import { fade } from 'svelte/transition';
+	import themeList from '../util/themes'
 </script>
 
 <div
 	class="scrollbar-hide relative flex  h-52 w-full items-center overflow-scroll bg-slate-800"
 	in:fade
 >
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
-	<Card />
+{#each themeList as item}
+	<Card id={item.id} name={item.name}/>
+{/each}
 </div>
