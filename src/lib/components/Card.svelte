1afd6d5714f9c1ce4043d12b88700f3b012c66cb
Mohit <mohitd2288@gmail.com>
2023-01-22 00:19:00 +0530

    added login form

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,18 +1,19 @@
 <script>
 	export var name, id;
 
-	import {userTheme} from '../util/stores';
-	const handleTheme = ({target})=>{
+	import { userTheme } from '../util/stores';
+	const handleTheme = ({ target }) => {
 		// console.log(target.id)
-		userTheme.update(()=> (target.id - 21));
-	}
+		userTheme.update(() => target.id - 21);
+	};
 </script>
+
 <div class=" mx-4 block min-h-[160px] min-w-[240px] cursor-pointer rounded-lg bg-white shadow-lg">
 	<div class="relative">
 		<img class="h-auto max-w-full" src="../../devstar.jpeg" alt="" />
 		<button
-		{id}
-			class="absolute top-0 flex h-full w-full items-center justify-center rounded-sm bg-slate-50 opacity-0 transition-all ease-in hover:opacity-80 text-xl font-bold"
+			{id}
+			class="absolute top-0 flex h-full w-full items-center justify-center rounded-sm bg-slate-50 text-xl font-bold opacity-0 transition-all ease-in hover:opacity-80"
 			on:click={handleTheme}
 		>
 			{name}
---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,61 @@
+<section class="absolute top-20 -z-40 h-screen w-screen bg-slate-50 md:top-0">
+	<div class="h-full px-6 text-gray-800">
+		<div
+			class="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between xl:justify-center"
+		>
+			<div class="shrink-1 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
+				<img src="../../signup.webp" class="w-full" alt="Login" />
+				<img src="../" alt="" srcset="" />
+			</div>
+			<div class="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:ml-20 xl:w-5/12">
+				<form>
+					<!-- Email input -->
+					<div class="mb-6">
+						<input
+							type="text"
+							class="m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+							placeholder="Email address"
+						/>
+					</div>
+
+					<!-- Password input -->
+					<div class="mb-6">
+						<input
+							type="password"
+							class="m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-4 py-2 text-xl font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+							placeholder="Password"
+						/>
+					</div>
+
+					<div class="mb-6 flex items-center justify-between">
+						<div>
+							<input
+								type="checkbox"
+								class=" float-left mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none"
+							/>
+							<label class=" inline-block text-gray-800" for="exampleCheck2">Remember me</label>
+						</div>
+						<a href="#!" class="text-gray-800">Forgot password?</a>
+					</div>
+
+					<div class="text-center lg:text-left">
+						<button
+							type="button"
+							class="inline-block rounded bg-blue-600 px-7 py-3 text-sm font-medium uppercase leading-snug text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+						>
+							Login
+						</button>
+						<p class="mt-2 mb-0 pt-1 text-sm font-semibold">
+							Don't have an account?
+							<a
+								href="#!"
+								class="text-red-600 transition duration-200 ease-in-out hover:text-red-700 focus:text-red-700"
+								>Register</a
+							>
+						</p>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
